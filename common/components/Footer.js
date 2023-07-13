512ddf8a0a398bb495cd026f5c6fbe70281166dc
Eugene <lin.eugene.l.e@gmail.com>
2023-07-13 20:23:14 +0800

    Add a link to at the footer

--- common/components/Footer.js
+++ common/components/Footer.js
@@ -1,4 +1,6 @@
 import styles from '../../styles/footer.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Footer(){
     return (
@@ -6,7 +8,12 @@ export default function Footer(){
             <footer>
             <div className={styles.footer_content}>
                 <p>&copy; 2023 DAU-Management App. All rights reserved.</p>
-                <p>Created by Yu-Chun Lin</p>
+                <span>Created by Yu-Chun Lin 
+                &nbsp;
+                <Link href="https://github.com/yuchun1214">
+                    <Image src="/github-mark.svg" width={30} height={30} alt='github profile'></Image>
+                </Link>
+                </span>
             </div>
             </footer>
         </>
